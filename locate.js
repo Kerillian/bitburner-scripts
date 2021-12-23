6eea1bc3c88a9646626bb3e7f6534e6c5486a1eb
Kerillian <49354481+Kerillian@users.noreply.github.com>
2021-12-23 05:10:37 -0500

    Add locate.js

---
+++ locate.js
@@ -0,0 +1,61 @@
+/** @param {NS} ns **/
+export async function main(ns)
+{
+	let target = ns.args[0] || "n00dles";
+
+	if (!ns.serverExists(target))
+	{
+		ns.tprint("ERROR: Cannot resolve hostname: " + target);
+		return;
+	}
+	
+	const path = traverse(ns, ns.getHostname(), target);
+
+	ns.tprintf("Starting at origin: " + ns.getHostname());
+	ns.tprintf(`Path : ${path.join(" -> ")}`);
+	ns.tprintf(`Cmds : ${path.map(x => `connect ${x}`).join("; ")}`);
+
+	return;
+}
+
+/** @param {NS} ns **/
+function traverse(ns, origin, target, cur_path=[])
+{
+	const path = cur_path.slice();
+	path.push(origin);
+
+	if (origin == target)
+	{
+		return path;
+	}
+	
+	const nodes = ns.scan(origin);
+
+	if (nodes.length === 0)
+	{
+		ns.print("failing because there is no possible path.");
+		return -1;
+	}
+	
+	while (nodes.length !== 0)
+	{
+		const node = nodes.pop();
+
+		if (path.includes(node))
+		{
+			continue;
+		}
+		else
+		{
+			let new_path = traverse(ns, node, target, path);
+
+			if (new_path != -1)
+			{
+				return new_path;
+			}
+		}
+	}
+
+	ns.print("failing because current tree failed.");
+	return -1;
+}
